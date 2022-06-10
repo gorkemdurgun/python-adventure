cfaa1ccbb6fa7ade5d1c5dc5a2a9b828d85c8afc
gorkemdurgunn <gorkemdurgunn@gmail.com>
2022-06-11 02:15:30 +0300

    .

--- work_in_class.py
+++ work_in_class.py
@@ -3,14 +3,28 @@ class Dog():
     hisOwner = 'Aaron Vela'
     
     def __init__(self, name, age, gender):
-        print('new dog added')
         self.name = name
         self.age = age
         self.gender = gender
+        
+    def getDogName(self):
+        print('my name is ' + self.name)
         
 
 dog1 = Dog('Poppy', 4, True)
 dog2 = Dog('Hyla', 1, False)
 
-print(dog1.name)
-print(dog1.hisOwner)
+
+#dog1.getDogName()
+#dog2.getDogName()
+
+class Cat():
+    
+    def __init__(self, age=0):
+        self.age = age
+        self.humanAge = age * 7
+        
+cat1 = Cat(5)
+
+print(cat1.age)
+print(cat1.humanAge)
